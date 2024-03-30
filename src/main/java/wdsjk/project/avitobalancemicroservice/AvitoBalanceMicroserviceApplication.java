6b959eb3c46f64b2f98c8bb834b45d0cdac09f80
wdsjk <mamaevivan543@gmail.com>
2024-03-30 22:56:37 +0300

    initial commit

---
+++ src/main/java/wdsjk/project/avitobalancemicroservice/AvitoBalanceMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package wdsjk.project.avitobalancemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvitoBalanceMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvitoBalanceMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wdsjk/project/avitobalancemicroservice/AvitoBalanceMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package wdsjk.project.avitobalancemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvitoBalanceMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
