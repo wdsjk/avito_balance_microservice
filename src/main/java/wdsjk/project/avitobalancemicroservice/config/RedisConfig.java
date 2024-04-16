81f35ca1f5565ade724debf75724f4f8e998d6ba
wdsjk <mamaevivan543@gmail.com>
2024-04-16 21:33:12 +0300

    caching with Redis for first additional task

---
+++ src/main/java/wdsjk/project/avitobalancemicroservice/config/RedisConfig.java
@@ -0,0 +1,41 @@
+package wdsjk.project.avitobalancemicroservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+
+import org.springframework.data.redis.core.RedisTemplate;
+
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.math.BigDecimal;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName("127.0.0.1"); // in learning purposes it's okay
+        redisStandaloneConfiguration.setPort(6379);
+
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, BigDecimal> redisTemplate() {
+        RedisTemplate<String, BigDecimal> redisTemplate = new RedisTemplate<>();
+
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new JdkSerializationRedisSerializer());
+        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+        redisTemplate.setEnableTransactionSupport(true);
+        redisTemplate.afterPropertiesSet();
+
+        return redisTemplate;
+    }
+}
---
+++ src/main/java/wdsjk/project/avitobalancemicroservice/service/impl/BalanceServiceImpl.java
@@ -0,0 +1,133 @@
+package wdsjk.project.avitobalancemicroservice.service.impl;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import wdsjk.project.avitobalancemicroservice.domain.Balance;
+import wdsjk.project.avitobalancemicroservice.dto.ShowRequest;
+import wdsjk.project.avitobalancemicroservice.dto.request.DepositRequest;
+import wdsjk.project.avitobalancemicroservice.dto.request.TransferRequest;
+import wdsjk.project.avitobalancemicroservice.dto.response.BalanceResponse;
+import wdsjk.project.avitobalancemicroservice.dto.request.WithdrawRequest;
+
+import wdsjk.project.avitobalancemicroservice.exception.InternalErrorException;
+import wdsjk.project.avitobalancemicroservice.exception.UserNotFoundException;
+import wdsjk.project.avitobalancemicroservice.repository.BalanceRepository;
+import wdsjk.project.avitobalancemicroservice.service.BalanceService;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.net.*;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+public class BalanceServiceImpl implements BalanceService {
+    private final BalanceRepository balanceRepository;
+    private final RedisTemplate<String, BigDecimal> redisTemplate;
+
+    @Override
+    public BalanceResponse deposit(DepositRequest request) {
+        Balance balance = balanceRepository.findByUserId(request.userId()).orElse(null);
+
+        if (null != balance) {
+            balance.setAmountOfMoney(balance.getAmountOfMoney().add(request.amountOfMoney()).setScale(2, RoundingMode.HALF_DOWN));
+        } else {
+            balance = Balance.builder()
+                        .id(UUID.randomUUID().toString())
+                        .userId(request.userId())
+                        .amountOfMoney(request.amountOfMoney().setScale(2, RoundingMode.HALF_DOWN))
+                    .build();
+        }
+
+        balanceRepository.save(balance);
+
+        return new BalanceResponse("Money has been successfully deposited");
+    }
+
+    @Override
+    @Transactional
+    public BalanceResponse withdraw(WithdrawRequest request) {
+        Balance balance = balanceRepository.findByUserId(request.userId()).orElseThrow(
+                () -> new UserNotFoundException(String.format("User with id: %s is not found!", request.userId()))
+        );
+
+        balance.setAmountOfMoney(balance.getAmountOfMoney().subtract(request.amountOfMoney()).setScale(2, RoundingMode.HALF_DOWN));
+
+        return new BalanceResponse("Money has been successfully withdrew");
+    }
+
+    @Override
+    @Transactional
+    public BalanceResponse transfer(TransferRequest request) {
+        Balance balanceFrom = balanceRepository.findByUserId(request.userFromId()).orElseThrow(
+                () -> new UserNotFoundException(String.format("User with id: %s is not found!", request.userFromId()))
+        );
+        Balance balanceTo = balanceRepository.findByUserId(request.userToId()).orElseThrow(
+                () -> new UserNotFoundException(String.format("User with id: %s is not found!", request.userToId()))
+        );
+
+        balanceFrom.setAmountOfMoney(balanceFrom.getAmountOfMoney().subtract(request.amountOfMoney()).setScale(2, RoundingMode.HALF_DOWN));
+        balanceTo.setAmountOfMoney(balanceTo.getAmountOfMoney().add(request.amountOfMoney()).setScale(2, RoundingMode.HALF_DOWN));
+
+        return new BalanceResponse("Money has been successfully transferred");
+    }
+
+    @Override
+    public BalanceResponse show(ShowRequest request, String currency) {
+        BigDecimal amount = balanceRepository.findByUserId(request.userId()).orElseThrow(
+                () -> new UserNotFoundException(String.format("User with id: %s is not found!", request.userId()))
+        ).getAmountOfMoney();
+
+        if (null != currency) {
+            BigDecimal exchangeRate = redisTemplate.opsForValue().get(currency);
+            if (null == exchangeRate) {
+                try (HttpClient client = HttpClient.newBuilder().build()) {
+                    HttpRequest req = HttpRequest
+                            .newBuilder(
+                                URI.create(
+                                    String.format("https://api.freecurrencyapi.com/v1/latest?" +
+                                            "base_currency=RUB&" +
+                                            "currencies=%s", currency)
+                                )
+                            )
+                            .header(
+                                    "apikey", "fca_live_B2FeYk8cwUtZhT2Q0CNSZSfuswYpsLMk3yfMwTQb" // api-key from https://freecurrencyapi.com
+                                                            // (better it was in some environment variable but for learning purposes it's okay)
+                            )
+                            .build();
+
+                    String response = client
+                            .sendAsync(req, HttpResponse.BodyHandlers.ofString()) // sending request
+                            .thenApply(HttpResponse::body) // getting body
+                            .join(); // returning value on complete
+
+                    JSONObject jsonObject = new JSONObject(response);
+
+                    if (!jsonObject.isNull("data")) {
+                        exchangeRate = jsonObject.getJSONObject("data").getBigDecimal(currency);
+                        amount = amount.multiply(exchangeRate).setScale(2, RoundingMode.HALF_DOWN);
+
+                        redisTemplate.opsForValue().set(currency, exchangeRate, 60, TimeUnit.SECONDS); // caching with TTL = 1 minute
+                    }
+                } catch (JSONException e) {
+                    throw new InternalErrorException();
+                }
+            } else
+                amount = amount.multiply(exchangeRate).setScale(2, RoundingMode.HALF_DOWN);
+        }
+
+        return new BalanceResponse(String.valueOf(amount));
+    }
+}
